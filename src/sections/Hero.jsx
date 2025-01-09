22c4ec874b6788b77f47810a4baab5241e66bbee
Abderrezague ismail <ki_abderrezague@esi.dz>
2025-01-09 22:22:41 +0100

    initial commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   const { t } = useTranslation()
   return (
     <section id='hero' className="section container bg-[url('/background1.png')] bg-cover bg-center h-auto mt-10">
-      <div className="relative  flex flex-col items-center justify-center text-3xl md:text-5xl gap-4 font-bold pt-8 pb-20">
+      <div className="relative  flex flex-col items-center justify-center text-2xl md:text-5xl gap-4 font-bold pt-8 pb-20">
         <p>{t('Hero.title.black')}<span className='text-primary pl-1'>{t('Hero.title.orange')}</span></p>
         <p>{t('Hero.subtitle.black')}<span className='text-primary pl-1'>{t('Hero.subtitle.orange')}</span></p>
         <span className='absolute right-8 top-0 bg-primary p-1 md:p-3 rounded-full flex items-center justify-center'>
